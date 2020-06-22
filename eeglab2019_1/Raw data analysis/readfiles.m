5ee8c7b65add92298e4e7588ad961e82c65bdfe2
Jmckone1 <36732640+Jmckone1@users.noreply.github.com>
2020-06-22 12:32:34 +0100

    included mat files, split code into read and run

--- eeglab2019_1/Raw data analysis/readfiles.m
+++ eeglab2019_1/Raw data analysis/readfiles.m
@@ -1,8 +1,8 @@
 clc; clear; close all;
 
 % input the event file
-Event_filename = "cba1ff02_events.csv";
-Data_filename = "cba1ff02_data.csv";
+Event_filename = "Event_files/cba1ff03_events.csv";
+Data_filename = "Data_files/cba1ff03_data.csv";
 
 writestats = 0;
 
@@ -76,89 +76,8 @@ for i = 2:m+1
         event_end = Events(i,3);
     end
 end
+
 if writestats == 1
     writematrix(info_matrix,"class/cba1ff05_info.csv");
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%                   code Run                  %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-test_channel_1 = Event_007; % event does not occur 
-test_channel_2 = Event_008; % event does occur
-fs = 1000;
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%           channel 1           %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% ----------------------------------- %
-% ------fast time fourier start------ %
-
-[L,C] = size(test_channel_1);
-
-run_raw_graphs(test_channel_1,"event 007");
-% output matrix for signal frequency and ampllitude
-x = size( run_fast_fourier_2(test_channel_1,fs,1));
-P1_output = zeros(x(2), C);
-F1_output = zeros(x(2), C);
-figure
-for v = 2:C
-    % apply fft to the signal
-    [f,P] = run_fast_fourier_2(test_channel_1,fs,v);
-    subplot(3,4,v-1);plot(f,P);title("Channel " + v);
-    % plot the frequency/amplitude fourier
-    P1_output(:,v-1) = P;
-    F1_output(:,v-1) = f;
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%           channel 2           %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-[L,C] = size(test_channel_2);
-
-run_raw_graphs(test_channel_2,"event 008");
-% output matrix for signal frequency and ampllitude
-x = size( run_fast_fourier_2(test_channel_2,1000,1));
-P2_output = zeros(x(2), C);
-F2_output = zeros(x(2), C);
-figure
-
-for v = 2:C
-    % apply fft to the signal
-    [f,P] = run_fast_fourier_2(test_channel_2,1000,v);
-    % plot the frequency/amplitude fourier
-    subplot(3,4,v-1);plot(f,P);title("Channel " + v);
-    P2_output(:,v-1) = P; % signal plot points
-    F2_output(:,v-1) = f; % amplitude plot points
-end
-
-% ----fast fourier transform end---- %
-% -----------------------------------%
-% -----short time fourier start----- %
-
-figure
-% for each channel
-for v = 2:C
-    % set base frame size
-    frame_size = 512;
-    subplot(3,4,v-1);plot(f,P);title("Channel " + v);
-    [f1,P11] = run_short_fourier(test_channel_1,fs,frame_size);
-    title(['spectogram of amplitude, frame size: ',num2str(frame_size)])
-    xlabel('Time, s'); ylabel('Frequency, Hz');
-end
-
-figure
-% for each channel
-for v = 2:C
-    % set base frame size
-    frame_size = 512;
-    subplot(3,4,v-1);plot(f,P);title("Channel " + v);
-    [f1,P11] = run_short_fourier(test_channel_2,fs,frame_size);
-    title(['spectogram of amplitude, frame size: ',num2str(frame_size)])
-    xlabel('Time, s'); ylabel('Frequency, Hz');
-end
-
-% ------short time fourier end------ %
-% ---------------------------------- %
--- eeglab2019_1/Raw data analysis/run_graphs.m
+++ eeglab2019_1/Raw data analysis/run_graphs.m
@@ -1,37 +1,82 @@
-function run_graphs(channel_name1, channel_name2)
-    
-    test_channel_1 = channel_name1;
-    test_channel_2 = channel_name2;
-    figure;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                   code Run                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    [x,~] = size(test_channel_1);
-    subplot(1,2,1);
-    plot(1:x,test_channel_1(:,:));
-    title("Test channel 1");
-    ylim([-350 350])
+test_channel_1 = Event_007; % event does not occur 
+test_channel_2 = Event_008; % event does occur
+fs = 1000;
 
-    %figure;
-    [x,~] = size(test_channel_2);
-    subplot(1,2,2);
-    plot(1:x,test_channel_2(:,:));
-    title("Test channel 2");
-    ylim([-350 350])
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%           channel 1           %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    figure
-    for v = 2:32
-        [x,~] = size(test_channel_1);
-        subplot(4,8,v-1);
-        plot(1:x,test_channel_1(:,v));
-        title("Channel " + v);
-        ylim([-350 350])
-    end
+% ----------------------------------- %
+% ------fast time fourier start------ %
 
-    figure
-    for v = 2:32
-        [x,~] = size(test_channel_2);
-        subplot(4,8,v-1);
-        plot(1:x,test_channel_2(:,v));
-        title("Channel " + v);
-        ylim([-350 350])
-    end
+[L,C] = size(test_channel_1);
+
+run_raw_graphs(test_channel_1,"event 007");
+% output matrix for signal frequency and ampllitude
+x = size( run_fast_fourier_2(test_channel_1,fs,1));
+P1_output = zeros(x(2), C);
+F1_output = zeros(x(2), C);
+figure
+for v = 2:C
+    % apply fft to the signal
+    [f,P] = run_fast_fourier_2(test_channel_1,fs,v);
+    subplot(3,4,v-1);plot(f,P);title("Channel " + v);
+    % plot the frequency/amplitude fourier
+    P1_output(:,v-1) = P;
+    F1_output(:,v-1) = f;
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%           channel 2           %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[L,C] = size(test_channel_2);
+
+run_raw_graphs(test_channel_2,"event 008");
+% output matrix for signal frequency and ampllitude
+x = size( run_fast_fourier_2(test_channel_2,1000,1));
+P2_output = zeros(x(2), C);
+F2_output = zeros(x(2), C);
+figure
+
+for v = 2:C
+    % apply fft to the signal
+    [f,P] = run_fast_fourier_2(test_channel_2,1000,v);
+    % plot the frequency/amplitude fourier
+    subplot(3,4,v-1);plot(f,P);title("Channel " + v);
+    P2_output(:,v-1) = P; % signal plot points
+    F2_output(:,v-1) = f; % amplitude plot points
+end
+
+% ----fast fourier transform end---- %
+% -----------------------------------%
+% -----short time fourier start----- %
+
+figure
+% for each channel
+for v = 2:C
+    % set base frame size
+    frame_size = 512;
+    subplot(3,4,v-1);plot(f,P);title("Channel " + v);
+    [f1,P11] = run_short_fourier(test_channel_1,fs,frame_size);
+    title(['spectogram of amplitude, frame size: ',num2str(frame_size)])
+    xlabel('Time, s'); ylabel('Frequency, Hz');
+end
+
+figure
+% for each channel
+for v = 2:C
+    % set base frame size
+    frame_size = 512;
+    subplot(3,4,v-1);plot(f,P);title("Channel " + v);
+    [f1,P11] = run_short_fourier(test_channel_2,fs,frame_size);
+    title(['spectogram of amplitude, frame size: ',num2str(frame_size)])
+    xlabel('Time, s'); ylabel('Frequency, Hz');
+end
+
+% ------short time fourier end------ %
+% ---------------------------------- %
