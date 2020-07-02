da3ecdaa292ad99e1b34e0116c973978acb9dd60
Jmckone1 <36732640+Jmckone1@users.noreply.github.com>
2020-07-02 17:14:17 +0100

    tested detrend on entire data - started window

--- eeglab2019_1/Raw data analysis/run_window.m
+++ eeglab2019_1/Raw data analysis/run_window.m
@@ -2,7 +2,6 @@ clear; clc; close all;
 % input the data file
 Data_filename = "Data_files/cba1ff01_data.csv";
 
-
 % read the csv file contents for the signal data
 Data = readmatrix(Data_filename);
 fs = 1000; % 
@@ -21,31 +20,24 @@ y = i - x;
 for a = 1:step_size:y+1
 
     figure;
-    subplot(2,1,1)
+    subplot(4,1,1)
     plot(1:window_size+1,test_channel_1(a:a+window_size));
     % FFT %
-    subplot(2,1,2)
+    subplot(4,1,2)
     [f,P] = run_fast_fourier_2(test_channel_1(a:a+window_size),fs,v);
     % plot the frequency/amplitude fourier
-
     plot(f,P);
-    % P1_output(:,v) = P;
-    % F1_output(:,v) = f;
+
+    frame_size = 128;
+    % STFT %
+    subplot(4,1,3);
+    [f1,p11] = run_short_fourier(test_channel_1(:,v),fs,frame_size);
+    legend(int2str(frame_size));
+    xlabel('Time (s)'); ylabel('Frequency, Hz');
+
+    % CWT %
+    subplot(4,1,4);
+    CWT_in(test_channel_1(:,v));
+    title("Channel " + v);
 
 end
-
-% 
-% frame_size = 64;
-% for j = 1:4
-%     % STFT %
-%     figure;
-%     [f1,p11] = run_short_fourier(test_channel_1(:,v),fs,frame_size);
-%     legend(int2str(frame_size));
-%     xlabel('Time (s)'); ylabel('Frequency, Hz');
-%     frame_size = frame_size * 2;
-% end
-% 
-% % CWT %
-% figure;
-% CWT_in(test_channel_1(:,v));
-% title("Channel " + v);
